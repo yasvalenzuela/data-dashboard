fa5aeb07454c52653ee8e57830e28bb979f8afb6
yasvalenzuela <yasna.valenzuela91@gmail.com>
2017-11-18 18:53:06 -0300

    estructura

--- js/app.js
+++ js/app.js
@@ -3,4 +3,16 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+//console.log(data);
+//var listita = document.getElementById('List');
+
+var iconoDesplegado = document.getElementById('iconoDesplegado');
+iconoDesplegado.addEventListener('click', function(){
+	document.getElementById('bienvenida').style.display = 'none';
+	document.getElementById('actualizacion').style.display = 'none';
+	document.getElementById('eventos').style.display = 'none';
+	document.getElementById('indicadores').style.display = 'none';
+	var desplegado = document.getElementById('desplegado');
+	desplegado.style.display = 'block';
+
+});
