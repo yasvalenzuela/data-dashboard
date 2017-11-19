b8c4016da02060633d2501a83366df7475f9db10
jotavasquez <jotavasquezjota@gmail.com>
2017-11-19 18:16:37 -0300

    boton y calendario popup-demo

--- js/app.js
+++ js/app.js
@@ -18,5 +18,22 @@ menuDesplegado.addEventListener('click', function(){
 	document.getElementById('indicadores').style.display = 'none';
 	var menuOculto = document.getElementById('menuOculto');
 	menuOculto.style.display = 'block';
-
 });
+
+
+// funcion que muestra el demo-calendario
+var btnCalendar = document.getElementById("date");
+
+function showDate() {
+	var calendar = document.getElementById('datePopUp');
+
+	if (calendar.classList.contains("hideCalendar")) {
+		calendar.classList.remove("hideCalendar");
+		menu.classList.add("seeCalendar");
+	} else {
+		console.log("ocultar");
+		calendar.classList.remove("seeCalendar");
+		calendar.classList.add("hideCalendar");
+	}
+}
+btnCalendar.addEventListener("click", showDate);
